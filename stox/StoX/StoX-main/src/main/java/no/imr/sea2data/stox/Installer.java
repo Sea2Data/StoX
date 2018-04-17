717b336f57c9429ef8b78d14d185262ce864e116
Aasmund Skaalevik <aasmunds@imr.no>
2018-04-17 15:26:21 +0200

    installer

--- stox/StoX/StoX-main/src/main/java/no/imr/sea2data/stox/Installer.java
+++ stox/StoX/StoX-main/src/main/java/no/imr/sea2data/stox/Installer.java
@@ -12,7 +12,7 @@ import org.openide.modules.ModuleInstall;
  */
 public class Installer extends ModuleInstall {
 
-    public static final String APPVERSION = "2.5";
+    public static final String APPVERSION = "2.6";
 
     @Override
     public void restored() {
