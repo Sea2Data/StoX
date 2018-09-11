6bd153aa3ff16655fd525c5b4a8053c5bf4b047b
aasmunds <aasmunds@HI-07679.imr.no>
2018-09-11 15:05:16 +0200

    stox-184 readacousticxml > 6 files

--- stox/StoX/StoX-main/src/main/java/no/imr/sea2data/stox/Installer.java
+++ stox/StoX/StoX-main/src/main/java/no/imr/sea2data/stox/Installer.java
@@ -12,7 +12,7 @@ import org.openide.modules.ModuleInstall;
  */
 public class Installer extends ModuleInstall {
 
-    public static final String APPVERSION = "2.6.2";
+    public static final String APPVERSION = "2.6.3";
 
     @Override
     public void restored() {
